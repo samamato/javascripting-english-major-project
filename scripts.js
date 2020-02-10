5546f6670a61443b8fd771fa7ef93f57a549fab3
Sam Amato <Sam@Sams-MacBook-Pro.local>
2020-02-10 11:57:31 -0500

    made scrpits.js interactive

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let userInput;
+userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
+if ( userInput === "burrito" ) {
+  $("#response").html("Brilliant choice!");
+} else {
+  $("#response").html("Don't you want a burrito?");
+}
