7faf2045b2ec53fb88063588fb93154019728fdb
Sam Amato <Sam@Sams-MacBook-Pro.local>
2020-03-04 11:55:18 -0500

    arrays

---
+++ practice.js
@@ -0,0 +1,14 @@
+let userInput;
+userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
+if (userInput === "burrito" || "Burrito" || "Burritos" || "burritos") {
+  $("#response").html("Brilliant choice!");
+} else {
+  $("#response").html("Don't you want a burrito?");
+}
+/*
+if ( userInput === "burrito" ) {
+$("#response").html("Brilliant choice!");
+  } else {
+ $("#response").html("Don’t you want a burrito?");
+  }
+*/
--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+/*
 let tipCalculator;
 tipCalculator = function(multiVariable, divVariable) {
   let multiTotal;
@@ -14,7 +15,7 @@ tipAmount = 0.2;
 let total;
 total = 50;
 tipCalculator();
-/*
+
 let makeABurrito;
 makeABurrito = function(beansVariable){
   let beansResponse;
@@ -25,3 +26,37 @@ let blackBeans;
 blackBeans = "black";
 makeABurrito(blackBeans);
 */
+/*
+let myHabaneroSauceSquirts, myBurritoObject;
+myHabaneroSauceSquirts = 3;
+myBurritoObject = {
+  tortilla: "wheat",
+  guacamole: true,
+  beans: "pinto",
+  habaneroSauceSquirts: myHabaneroSauceSquirts,
+  spiciness: function(){
+    if (myHabaneroSauceSquirts > 0 ){
+      alert("This is a spicy burrito!");
+    } else {
+      alert("This is a mild burrito.");
+    }
+  }
+};
+$("#response").html("Your burrito has " +
+  myBurritoObject.habaneroSauceSquirts +
+  " squirts of habanero.");
+myBurritoObject.spiciness();
+*/
+/*
+let turtles, sortedTurtles, reversedTurtles, turtleNames;
+turtles = ["Leonardo", "Donatello", "Raphael", "Michelangelo"];
+sortedTurtles = turtles.sort();
+reversedTurtles = turtles.reverse();
+turtleNames = turtles.join(" ");
+turtles.push("Splinter");
+turtles.pop();
+*/
+let animal;
+animal = ["dog", "cat", "bird", "hamster", "rat"];
+let lastanimal=animal.pop();
+$("#response").html(lastanimal);
