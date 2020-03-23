865a1144c154d4dcb68b3da84411c07d6e23d88a
Sam Amato <Sam@sams-mbp.myfiosgateway.com>
2020-03-23 11:57:01 -0400

    finished working site

--- prologue.js
+++ prologue.js
@@ -1,6 +1,43 @@
 $("#prologue").html("<p>The text of the Prologue will go here.</p>");
 $("#glosses").html("<p>The glosses will go here.</p>");
-let line1, line1Text; // don’t need the intermediate step of line1TextArray
+$.getJSON("https://the-javascripting-english-major.org/v1/prologue.json", function(data){ // Note the data variable!
+  let prologueText; // Define the variable you didn’t need before.
+  prologueText = "<blockquote><p>"; // Open the tags.
+  // Now you can iterate over the data variable’s .lines property:
+  data.lines.forEach(function(line){ // We get a variable, line.
+    // Define a blank lineText.
+    let lineText;
+    lineText = "";
+    // Now iterate over each line. This part should be familiar.
+    line.forEach(function(word){
+      let wordString;
+      wordString = word.text;
+      if (word.modern){
+        wordString = "<a href='#' data-modern='" + word.modern + "'>" + wordString + "</a>";
+      }
+      lineText = lineText + wordString + " ";
+    });
+    // Add lineText with a line break to the prologueText.
+    prologueText = prologueText + lineText + "<br/>";
+  });
+  // Close the prologueText tags.
+  prologueText = prologueText + "</p></blockquote>";
+  // Replace the content of #prologue.
+  $("#prologue").html(prologueText);
+  $("#prologue a").click(function(){
+    let glossText, clickedWord, modernWord;
+    clickedWord = $( this ).text();
+    // .data("modern") looks for the data-modern HTML attribute.
+    modernWord = $( this ).data("modern");
+    glossText = "<h2>You clicked on " + clickedWord + ", which means " + modernWord +"</h2>";
+    $("#glosses").html(glossText);
+});
+});
+
+
+// Close the callback function & method.
+
+ /* let line1, line1Text; // don’t need the intermediate step of line1TextArray
 line1 = [{text: "Whan", modern: "When"}, {text: "that"}, {text: "Aprill,",
         modern: "April,"}, {text: "with"}, {text: "his"}, {text: "shoures",
         modern: "showers"}, {text: "soote", modern: "sweet"}];
@@ -27,3 +64,4 @@ $("#prologue a").click(function(){
   glossText = "<h2>You clicked on the word: " + clickedWord + "</h2>";
   $("#glosses").html(glossText);
 });
+*/
